d7a0c51f877eba81ad01444c06a4f5c0b111ed37
bayufadayan <bayufadayan@gmail.com>
2025-09-05 01:22:04 +0700

    feat: adjust login view

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -3,6 +3,8 @@ import { home } from '@/routes';
 import { type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { register } from '@/routes';
 
 interface AuthLayoutProps {
     title?: string;
@@ -14,32 +16,65 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
 
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
-                <Link href={home()} className="relative z-20 flex items-center text-lg font-medium">
+            <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex">
+                <div className="absolute inset-0 bg-zinc-900">
+                    <figure className="relative h-full w-full overflow-hidden">
+                        <LazyLoadImage
+                            alt="Banner Starter 1"
+                            src="/images/banner-starter-1.svg"
+                            className="h-full w-full object-cover object-bottom transition-transform duration-500"
+                        />
+                    </figure>
+                </div>
+                <Link href={home()} className="relative z-20 flex items-center text-lg font-medium flex-col text-[#F2FF8F]">
                     <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
                     {name}
                 </Link>
+                <p className='relative z-20 text-center text-base mt-8 text-[#00FF80] px-24'>
+                    Laraventra adalah museum virtual interaktif yang menampilkan koleksi barang absurd dalam galeri digital penuh warna dan imajinasi.
+                </p>
                 {quote && (
                     <div className="relative z-20 mt-auto">
-                        <blockquote className="space-y-2">
+                        <blockquote className="space-y-1 text-[#b1ffd1]">
                             <p className="text-lg">&ldquo;{quote.message}&rdquo;</p>
-                            <footer className="text-sm text-neutral-300">{quote.author}</footer>
+                            <footer className="text-sm">— {quote.author}</footer>
                         </blockquote>
                     </div>
                 )}
             </div>
-            <div className="w-full lg:p-8">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="relative w-full flex flex-col justify-between items-center h-full py-4">
+                <div className="absolute inset-0">
+                    <figure className="relative h-full w-full overflow-hidden">
+                        <LazyLoadImage
+                            alt="Banner Starter 2"
+                            src="/images/banner-starter-2.svg"
+                            className="h-full w-full object-cover object-bottom transition-transform duration-500"
+                        />
+                    </figure>
+                </div>
+                <div className="absolute inset-0">
+                    <figure className="relative h-full w-full overflow-hidden">
+                        <LazyLoadImage
+                            alt="Banner Starter 3"
+                            src="/images/banner-starter-3.svg"
+                            className="h-full w-full mix-blend-lighten object-cover object-bottom transition-transform duration-500"
+                        />
+                    </figure>
+                </div>
+                <Link href={register()} className='bg-[#00f8be]/10 text-[#005B46] border border-[#005B46] px-6 py-2 self-end mx-8 z-20'>
+                    Register
+                </Link>
+                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] z-20">
                     <Link href={home()} className="relative z-20 flex items-center justify-center lg:hidden">
                         <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
                     </Link>
-                    <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
-                        <h1 className="text-xl font-medium">{title}</h1>
-                        <p className="text-sm text-balance text-muted-foreground">{description}</p>
+                    <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center text-[#005B46]">
+                        <h1 className="text-2xl font-bold">{title}</h1>
+                        <p className="text-sm text-balance">{description}</p>
                     </div>
                     {children}
                 </div>
+                <p className='text-center text-sm w-[350px] text-[#005B46] z-20'>By clicking continue, you agree to our <span className='underline'>Terms of Service</span> and <span className='underline'>Privacy Policy</span>.</p>
             </div>
         </div>
     );
--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -6,10 +6,11 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import { register } from '@/routes';
 import { request } from '@/routes/password';
 import { Form, Head } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
+import { MdAccountCircle } from "react-icons/md";
+import { GiPadlock } from "react-icons/gi";
 
 interface LoginProps {
     status?: string;
@@ -24,59 +25,65 @@ export default function Login({ status, canResetPassword }: LoginProps) {
             <Form {...AuthenticatedSessionController.store.form()} resetOnSuccess={['password']} className="flex flex-col gap-6">
                 {({ processing, errors }) => (
                     <>
-                        <div className="grid gap-6">
-                            <div className="grid gap-2">
-                                <Label htmlFor="email">Email address</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    name="email"
-                                    required
-                                    autoFocus
-                                    tabIndex={1}
-                                    autoComplete="email"
-                                    placeholder="email@example.com"
-                                />
+                        <div className="grid gap-3">
+                            <div className="grid gap-2 text-[#005B46]">
+                                <div className="relative">
+                                    <div className='absolute text-[#005B46] left-2 flex h-full items-center'>
+                                        <MdAccountCircle className='size-8' />
+                                    </div>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                        autoFocus
+                                        tabIndex={1}
+                                        autoComplete="email"
+                                        placeholder="email@example.com"
+                                        className='border border-[#005B46] pl-12 py-5 rounded-full placeholder:text-[#005B46]/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#00f8be] focus-visible:ring-opacity-10 focus-visible:border-[#00f8be] selection:bg-[#00f8be] selection:text-[#005B46]'
+                                    />
+                                </div>
                                 <InputError message={errors.email} />
                             </div>
 
-                            <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    {canResetPassword && (
-                                        <TextLink href={request()} className="ml-auto text-sm" tabIndex={5}>
-                                            Forgot password?
-                                        </TextLink>
-                                    )}
+                            <div className="grid gap-2 text-[#005B46]">
+                                <div className="relative">
+                                    <div className='absolute text-[#005B46] left-2.5 flex h-full items-center'>
+                                        <GiPadlock className='size-7' />
+                                    </div>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        required
+                                        tabIndex={2}
+                                        autoComplete="current-password"
+                                        placeholder="Password"
+                                        className='border border-[#005B46] pl-12 py-5 rounded-full placeholder:text-[#005B46]/70 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#00f8be] focus-visible:ring-opacity-10 focus-visible:border-[#00f8be] selection:bg-[#00f8be] selection:text-[#005B46]'
+                                    />
                                 </div>
-                                <Input
-                                    id="password"
-                                    type="password"
-                                    name="password"
-                                    required
-                                    tabIndex={2}
-                                    autoComplete="current-password"
-                                    placeholder="Password"
-                                />
                                 <InputError message={errors.password} />
                             </div>
 
-                            <div className="flex items-center space-x-3">
-                                <Checkbox id="remember" name="remember" tabIndex={3} />
-                                <Label htmlFor="remember">Remember me</Label>
+                            <div className="flex items-center space-x-2 text-[#005B46]">
+                                <Checkbox id="remember" name="remember" tabIndex={3} className='border-[#005B46]'/>
+                                <Label htmlFor="remember" className='text-[#005B46]'>Remember me</Label>
                             </div>
 
-                            <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
-                                {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                                Log in
-                            </Button>
-                        </div>
-
-                        <div className="text-center text-sm text-muted-foreground">
-                            Don't have an account?{' '}
-                            <TextLink href={register()} tabIndex={5}>
-                                Sign up
-                            </TextLink>
+                            <div className='flex flex-col gap-2'>
+                                <Button type="submit" className="mt-4 w-full bg-[#005B46] text-white" tabIndex={4} disabled={processing}>
+                                    {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                                    Log in
+                                </Button>
+                                <div className="flex flex-row text-xs justify-center gap-1 text-[#005B46]">
+                                    <p>Forgot password?</p>
+                                    {canResetPassword && (
+                                        <TextLink href={request()} className="text-[#005B46]" tabIndex={5}>
+                                            Reset Now
+                                        </TextLink>
+                                    )}
+                                </div>
+                            </div>
                         </div>
                     </>
                 )}
